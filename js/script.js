6135448913f0629abad63ef8f51664400f4da727
Christopher Stone <cstone@hubspot.com>
2016-05-28 10:49:40 -0400

    adding inital project files

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// <ul class="student-list">
+//   <li class="student-item cf">
+//       <div class="student-details">
+//           <img class="avatar" src="https://randomuser.me/api/portraits/thumb/women/67.jpg">
+//           <h3>iboya vat</h3>
+//           <span class="email">iboya.vat@example.com</span>
+//       </div>
+//       <div class="joined-details">
+//              <span class="date">Joined 07/15/15</span>
+//      </div>
+//   </li>
+// </ul>
